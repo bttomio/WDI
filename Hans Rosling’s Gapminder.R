f6507a087e5a63a10c5d5ed312259e4f9c4a5ddb
Bruno Tomio <44265732+bttomio@users.noreply.github.com>
2020-11-18 15:43:32 +0100

    First version - ready

---
+++ Hans Rosling’s Gapminder.R
@@ -0,0 +1,111 @@
+library(tidyverse)
+library(WDI)
+
+indicators <- c(
+  life_exp = "SP.DYN.LE00.IN", 
+  gdp_capita ="NY.GDP.PCAP.CD", 
+  pop = "SP.POP.TOTL"
+)
+
+hrg <- WDI(indicators, country="all", start = "2018", end = "2018")
+Data_info <- WDI_data
+Data_countries <- as.data.frame(Data_info$country) 
+
+hrg %>%
+  left_join(Data_countries, "iso2c") %>%
+  filter(region != "Aggregates") %>% # remove aggregates (groups of countries)
+  ggplot() +
+  geom_point(
+    aes(
+      x = gdp_capita, 
+      y = life_exp, 
+      size = pop, 
+      color = region
+    )
+  ) +
+  scale_x_continuous(
+    labels = scales::dollar_format(),
+    breaks = scales::log_breaks(n = 10)
+  ) +
+  coord_trans(x = 'log10') +
+  scale_size_continuous(
+    labels = scales::number_format(scale = 1/1e6, suffix = "m"),
+    breaks = seq(1e8,1e9, 2e8),
+    range = c(1,20)
+  ) +
+  theme_minimal() +
+  labs(
+    title = "An Example of Hans Rosling's Gapminder using WDI",
+    x = "GDP per capita (log scale)",
+    y = "Life expectancy at birth",
+    size = "Population",
+    color = NULL,
+    caption = "Source: World Bank"
+  ) 
+
+##### HRG 2 ####
+
+library(ggrepel)
+
+hrg2 <- hrg %>%
+  left_join(Data_countries, "iso2c") %>%
+  filter(region != "Aggregates") # remove aggregates (groups of countries)
+
+ggplot(hrg2) +
+  geom_point(
+    aes(
+      x = gdp_capita, 
+      y = life_exp, 
+      size = pop, 
+      color = region
+    )
+  ) +
+  scale_x_continuous(
+    labels = scales::dollar_format(),
+    breaks = scales::log_breaks(n = 10)
+  ) +
+  coord_trans(x = 'log10') +
+  scale_size_continuous(
+    labels = scales::number_format(scale = 1/1e6, suffix = "m"),
+    breaks = seq(1e8,1e9, 2e8),
+    range = c(1,20)
+  ) +
+  theme_minimal() +
+  labs(
+    title = "An Example of Hans Rosling's Gapminder using WDI",
+    x = "GDP per capita (log scale)",
+    y = "Life expectancy at birth",
+    size = "Population",
+    color = NULL,
+    caption = "Source: World Bank"
+  ) +
+  geom_label_repel(data = subset(hrg2, life_exp > 83 | life_exp < 55),
+                   aes(x = gdp_capita, y = life_exp, label = country.x),
+                   box.padding   = 0.35, 
+                   point.padding = 0.5,
+                   segment.color = 'grey50')
+
+##### HRG 3 #####
+
+ggplot(hrg2) +
+  geom_point(
+    aes(x = gdp_capita, y = life_exp, size = pop, color = region)) +
+  scale_x_continuous(
+    labels = scales::dollar_format(),
+    breaks = scales::log_breaks(n = 10)) +
+  coord_trans(x = 'log10') +
+  scale_size_continuous(
+    labels = scales::number_format(scale = 1/1e6, suffix = "m"),
+    breaks = seq(1e8,1e9, 2e8),
+    range = c(1,20)) +
+  theme_minimal() +
+  labs(x = "GDP per capita (log scale)",
+       y = "Life expectancy at birth",
+       size = "Population",
+       color = NULL,
+       caption = "Source: World Bank") +
+  geom_label_repel(data = subset(hrg2, pop > 90000000), # 90 millions
+                   aes(x = gdp_capita, y = life_exp, label = country.x),
+                   box.padding   = 0.9,
+                   point.padding = 0.9,
+                   segment.color = 'grey50')
